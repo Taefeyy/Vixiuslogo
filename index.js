f5a31db1ced9e4b650b5559bf5e4413cb3f79cdd
Kellium <95094110+Taefeyy@users.noreply.github.com>
2022-10-25 21:12:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.login(process.env.TOKEN); // à remettre pour heroku
 mongoose.connect(process.env.DBTOKEN, {
     autoIndex: false, // Don't build indexes
     maxPoolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    serverSelectionTimeoutMS: 50000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 450000, // Close sockets after 45 seconds of inactivity
     family: 4 // Use IPv4, skip trying IPv6
   }).then(()=>{console.log('Le client est connecté à la DB')}).catch(err => {console.log(err)});
